70308c2389bc77a6be0f71f36d266d8283451d4d
Kaio <kaio.correia@alunoescolassociais.org.br>
2024-09-20 09:24:10 -0300

    verificação dos caracteres CEP e função API

--- form.js
+++ form.js
@@ -11,4 +11,14 @@ const limparFormulario = () => {
     document.getElementById(Complemento).value = "";
     document.getElementById(uf).value = "";
 }
+const eNumero = (numero) => /^[0-9]+$/.test(numero); const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+/// length verifica a quantidade de caracteres do argumento cep
 
+// função para preencher formulario com campos da API
+
+const preencherFormulario = (endereco) => {
+    document.getElementById('logradouro').value = endereco.logradouro;
+    document.getElementById('localidade').value = endereco.localidade;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('uf').value = endereco.uf;
+}
