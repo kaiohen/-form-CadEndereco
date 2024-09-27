d2fd294728c8af57cf14c2ff72f0dc753f2ff719
Kaio <kaio.correia@alunoescolassociais.org.br>
2024-09-27 08:17:16 -0300

    correção erros

--- form.js
+++ form.js
@@ -1,24 +1,24 @@
-'use strict'; // Modo restrito
+"use strict"; // Modo restrito
 // Este modo Ajuda a evitar erros comuns de programação, JavaScript opera de forma mais segura e rigorosa
-/* consumo de API - https://viacep.com.br/ */ 
-
+/* consumo de API - https://viacep.com.br/ */
 // função limpar formulario
 const limparFormulario = () => {
-    document.getElementById(logradouro).value = "";
-    document.getElementById(numero).value = "";
-    document.getElementById(bairro).value = "";
-    document.getElementById(localidade).value = "";
-    document.getElementById(Complemento).value = "";
-    document.getElementById(uf).value = "";
-}
-const eNumero = (numero) => /^[0-9]+$/.test(numero); const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+  document.getElementById("logradouro").value = "";
+  document.getElementById("numero").value = "";
+  document.getElementById("bairro").value = "";
+  document.getElementById("localidade").value = "";
+  document.getElementById("Complemento").value = "";
+  document.getElementById("uf").value = "";
+};
+const eNumero = (numero) => /^[0-9]+$/.test(numero);
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 /// length verifica a quantidade de caracteres do argumento cep
 
 // função para preencher formulario com campos da API
-
 const preencherFormulario = (endereco) => {
-    document.getElementById('logradouro').value = endereco.logradouro;
-    document.getElementById('localidade').value = endereco.localidade;
-    document.getElementById('bairro').value = endereco.bairro;
-    document.getElementById('uf').value = endereco.uf;
-}
+  document.getElementById("logradouro").value = endereco.logradouro;
+  document.getElementById("bairro").value = endereco.bairro;
+  document.getElementById("localidade").value = endereco.localidade;
+  document.getElementById("Complemento").value = endereco.Complemento;
+  document.getElementById("uf").value = endereco.uf;
+};
